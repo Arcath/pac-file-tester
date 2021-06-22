58d5082f6aedd7b0712a71043fb5bff5805e5540
fc-rollsd <daniel.rolls@flightcentre.com.au>
2021-06-22 12:15:52 +1000

    Add dnsDomainIs & fix broken getHost.

--- src/pac-file-tester.ts
+++ src/pac-file-tester.ts
@@ -30,11 +30,8 @@ export const testPacFile = async (file: string, url: string, ip: string = addres
 }
 
 export const getHost = (url: string): string => {
-  const regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im
-
-  const match = regex.exec(url)
-
-  return match[1]
+  const urlObj = new URL(url);
+  return urlObj.hostname
 }
 
 const vmContext = (ip: string) => {
@@ -95,13 +92,19 @@ const vmContext = (ip: string) => {
     return host.indexOf(".") === -1;
   }
 
+  const dnsDomainIs = (host: string, domain: string) => {
+    console.log(`${host} compare to ${domain}`);
+    return host.endsWith(domain);
+  }
+
   return vm.createContext({
     console: console,
     myIpAddress,
     shExpMatch,
     isInNet,
     dnsResolve,
-    isPlainHostName
+    isPlainHostName,
+    dnsDomainIs
   })
 }
 
